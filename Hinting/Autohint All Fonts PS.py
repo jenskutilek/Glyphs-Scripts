cf67d88c9dc7e27f6ad6180330f8ca79baa00bc9
Jens Kutilek <webmail@kutilek.de>
2017-09-21 17:58:56 +0200

    Only hint the first layer

--- Hinting/Autohint All Fonts PS.py
+++ Hinting/Autohint All Fonts PS.py
@@ -4,6 +4,7 @@ print "Autohinting ..."
 for f in Glyphs.fonts:
 	print "    %s" % f
 	for g in Glyphs.font.glyphs:
-		for l in g.layers:
-			l.autohint()
+		#for l in g.layers:
+		#	l.autohint()
+		g.layers[0].autohint()
 
