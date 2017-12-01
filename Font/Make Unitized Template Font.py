2b09d76f2ed0eb0b700f439293459a555b318373
Jens Kutilek <webmail@kutilek.de>
2017-12-01 19:13:03 +0100

    Add script "Make Unitized Template Font"

---
+++ Font/Make Unitized Template Font.py
@@ -0,0 +1,20 @@
+#MenuTitle: Make Unitized Template Font
+
+from jkRFoTools.unitization import unitization_info
+
+sc = unitization_info.get_systems_by_upm(9)[0] # IBM Selectric Composer, 3-9 units
+
+print sc.fixed_units
+
+# 900 upm are assumed for now
+upm = Glyphs.font.upm
+
+for u, chars in sc.fixed_units.items():
+	for c in chars:
+		g = GSGlyph("uni%04X" % ord(c))
+		try:
+			Glyphs.font.glyphs.append(g)
+		except:
+			g = Glyphs.font.glyphs[c]
+		g.unicode = hex(ord(c))
+		g.layers[0].width = u * 100
