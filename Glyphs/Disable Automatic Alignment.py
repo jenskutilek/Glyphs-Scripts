a9535a2ac9a17f347a3f453a4ce71f8ade0ae1ed
Jens Kutilek <webmail@kutilek.de>
2018-01-19 16:51:24 +0100

    Add script "Glyphs > Disable Automatic Alignment"

---
+++ Glyphs/Disable Automatic Alignment.py
@@ -0,0 +1,10 @@
+glyphs = [l.parent for l in Glyphs.font.selectedLayers]
+
+Glyphs.font.disableUpdateInterface()
+
+for g in glyphs:
+	for l in g.layers:
+		for c in l.components:
+			c.automaticAlignment = False
+	
+Glyphs.font.enableUpdateInterface()
