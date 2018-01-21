c90d6560d3702879a5893fa71d0fb8f4bbe8dbe9
Jens Kutilek <webmail@kutilek.de>
2018-01-21 14:33:05 +0100

    Add Enable Automatic Alignment script

---
+++ Glyphs/Enable Automatic Alignment.py
@@ -0,0 +1,13 @@
+for sl in Glyphs.font.selectedLayers:
+	g = sl.parent
+	for l in g.layers:
+		for i in range(len(l.components)):
+			c = l.components[i]
+			c.automaticAlignment = True
+			c.position = (0, -402)
+		l.leftMetricsKey = None
+		l.rightMetricsKey = None
+	#g.leftMetricsKey = l.components[0].componentName
+	#g.rightMetricsKey = l.components[0].componentName
+	g.leftMetricsKey = None
+	g.rightMetricsKey = None
