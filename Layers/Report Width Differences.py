0af6c72b43503c02701137341fa0e2ce43788ebb
Jens Kutilek <webmail@kutilek.de>
2018-08-07 23:56:36 +0200

    Add "Report Width Differences"

---
+++ Layers/Report Width Differences.py
@@ -0,0 +1,26 @@
+# MenuTitle: Report Width Differences
+
+sf = Glyphs.font
+sm = sf.selectedFontMaster
+
+for tf in Glyphs.fonts:
+	if tf != sf:
+		tm = tf.selectedFontMaster
+		break
+
+missing = []
+
+for g in sf.glyphs:
+	sl = g.layers[sm.id]
+	if g.name in tf.glyphs:
+		tg = tf.glyphs[g.name]
+		tl = tg.layers[tf.selectedFontMaster.id]
+		if sl.width != tl.width:
+			print "%s;%s;%s;" % (g.name, sl.width, tl.width)
+	else:
+		#	print "%s;%s;;" % (g.name, " ".join(sc))
+		if not (g.name.endswith(".sc") or g.name.endswith(".pcap")):
+			missing.append(g.name)
+
+print "\nMissing in target:\n"
+print "\n".join(missing)
