3dc0cb8500a6402d97bf5d323d379bac4e03b26d
Jens Kutilek <webmail@kutilek.de>
2018-09-03 12:56:20 +0200

    Add Layers/Skew Paths and Anchors only (by Georg from forum)

---
+++ Layers/Skew Paths.py
@@ -0,0 +1,27 @@
+#MenuTitle: Skew only Paths and Anchors
+# -*- coding: utf-8 -*-
+__doc__="""
+"""
+
+import GlyphsApp
+import math 
+from AppKit import NSAffineTransform
+
+def slantLayers(layers, angle):
+	Font.disableUpdateInterface()
+	xHeight = layers[0].associatedFontMaster().xHeight
+	transform = NSAffineTransform.new()
+	slant = math.tan(skewAngle * math.pi / 180.0)
+	transform.shearXBy_atCenter_(slant, -xHeight / 2.0)
+	
+	for layer in selectedLayers:
+		for path in layer.paths:
+			for node in path.nodes:
+				node.position = transform.transformPoint_(node.position)
+		for anchor in layer.anchors:
+			anchor.position = transform.transformPoint_(anchor.position)
+		
+	Font.enableUpdateInterface()
+
+skewAngle = 13
+slantLayers(Glyphs.font.selectedLayers, skewAngle)
