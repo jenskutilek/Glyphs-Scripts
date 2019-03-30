ce697304ca38deddb2b3c52d85c4237ac0cf00a0
Jens Kutilek <webmail@kutilek.de>
2019-03-30 10:11:16 +0100

    Don't set position when enabling automatic alignment

--- Glyphs/Enable Automatic Alignment.py
+++ Glyphs/Enable Automatic Alignment.py
@@ -1,15 +1,15 @@
 #MenuTitle: Enable Automatic Alignment (Font-specific)
 
 for sl in Glyphs.font.selectedLayers:
-	g = sl.parent
-	for l in g.layers:
-		for i in range(len(l.components)):
-			c = l.components[i]
-			c.automaticAlignment = True
-			c.position = (0, -402)
-		l.leftMetricsKey = None
-		l.rightMetricsKey = None
-	#g.leftMetricsKey = l.components[0].componentName
-	#g.rightMetricsKey = l.components[0].componentName
-	g.leftMetricsKey = None
-	g.rightMetricsKey = None
+    g = sl.parent
+    for l in g.layers:
+        for i in range(len(l.components)):
+            c = l.components[i]
+            c.automaticAlignment = True
+            # c.position = (0, -402)
+        l.leftMetricsKey = None
+        l.rightMetricsKey = None
+    # g.leftMetricsKey = l.components[0].componentName
+    # g.rightMetricsKey = l.components[0].componentName
+    g.leftMetricsKey = None
+    g.rightMetricsKey = None
