c11cd43e3916c358644dcbe778285298cd037141
Jens Kutilek <webmail@kutilek.de>
2019-04-15 11:39:15 +0200

    Implement the splitting

--- Effects/Flatten Glyph.py
+++ Effects/Flatten Glyph.py
@@ -1,6 +1,7 @@
 # MenuTitle: Flatten Glyph MM
 from __future__ import absolute_import, division, print_function
 
+from fontPens.penTools import getCubicPoint
 from fontTools.misc.bezierTools import calcCubicArcLength
 from math import ceil, sqrt
 
@@ -19,12 +20,12 @@ def measureGlyph(glyph):
 	"""
 	lengths = {}
 	# Reference layer, just used for counting paths and segments
-	ref_layer = glyph.layers[masters[master_names[0]]]
+	ref_layer = glyph.layers[masters[master_names[0]]].background
 	for i in range(len(ref_layer.paths)):
 		for j in range(len(ref_layer.paths[i].segments)):
 			segment_lengths = []
 			for master_name in master_names:
-				layer = glyph.layers[masters[master_name]]
+				layer = glyph.layers[masters[master_name]].background
 				s = layer.paths[i].segments[j]
 				if len(s) == 4:
 					# curve
@@ -48,6 +49,7 @@ def measureGlyph(glyph):
 def calculateFlatNumbers(segment_lengths, length=30):
 	"""Calculate the number of flat segments each segment should be split into.
 	Each resulting segment should have approximately the given length.
+	The returned list is sorted in reverse to make further processing easier.
 	"""
 	return [
 		int(ceil(max(segment_lengths[k]) / length))
@@ -55,7 +57,52 @@ def calculateFlatNumbers(segment_lengths, length=30):
 	]
 
 
+def getLinearPoint(t, p1, p2):
+	return (
+		p1[0] + t * (p2[0] - p1[0]),
+		p1[1] + t * (p2[1] - p1[1])
+	)
+
+
+def splitSegments(glyph, split_counts):
+	"""Do the actual splitting.
+	"""
+	for master_name in master_names:
+		layer = glyph.layers[masters[master_name]]
+		counts = split_counts[:]
+		pen = layer.getPen()
+		for path in layer.background.paths:
+			pen.beginPath()
+			pt = path.segments[0][0]
+			pen.moveTo((pt.x, pt.y))
+			for segment in path.segments:
+				if len(segment) == 4:
+					split_function = getCubicPoint
+					p1, p2, p3, p4 = segment
+					args = [
+						(p1.x, p1.y),
+						(p2.x, p2.y),
+						(p3.x, p3.y),
+						(p4.x, p4.y),
+					]
+				elif len(segment) == 2:
+					split_function = getLinearPoint
+					p1, p2 = segment
+					args = [
+						(p1.x, p1.y),
+						(p2.x, p2.y),
+					]
+
+				count = counts.pop()
+				for i in range(count):
+					t = i / count
+					pt = split_function(t, *args)
+					pen.lineTo(pt)
+			pen.closePath()
+
+
 arc_lengths = measureGlyph(Font.selectedLayers[0].parent)
 print(arc_lengths)
-splits = calculateFlatNumbers(arc_lengths, 10)
+splits = calculateFlatNumbers(arc_lengths, 20)
 print(splits)
+splitSegments(Font.selectedLayers[0].parent, splits)
