7e72dd60e6bf6be7e5ac046b0aa95d925209ab22
Jens Kutilek <webmail@kutilek.de>
2020-10-12 10:49:53 +0200

    Add MenuTitle so scripts show up in the menu

--- Glyphs/Add All Possible Glyphs.py
+++ Glyphs/Add All Possible Glyphs.py
@@ -1,3 +1,11 @@
+# MenuTitle: Add All Possible Glyphs
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
+
 infos = GSGlyphsInfo.alloc().init()
 font = Glyphs.font
 
@@ -5,28 +13,35 @@ build_glyphs = False
 names = []
 
 for info in infos.glyphInfos():
-	if not info.components or info.name in font.glyphs or info.name.endswith(".half") or info.name.endswith(".full") or info.name.endswith("mod") or info.name.endswith("-math"):
-		continue
+    if (
+        not info.components
+        or info.name in font.glyphs
+        or info.name.endswith(".half")
+        or info.name.endswith(".full")
+        or info.name.endswith("mod")
+        or info.name.endswith("-math")
+    ):
+        continue
 
-	component_missing = False
-	for c in info.components:
-		if c.name not in font.glyphs:
-			# print "Skipping %s because of missing component %s..." % (info.name, c.name)
-			component_missing = True
-			break
+    component_missing = False
+    for c in info.components:
+        if c.name not in font.glyphs:
+            # print "Skipping %s because of missing component %s..." % (info.name, c.name)
+            component_missing = True
+            break
 
-	if component_missing:
-		continue
-	
-	if build_glyphs:
+    if component_missing:
+        continue
 
-		glyph = GSGlyph(info.name)
-		font.glyphs.append(GSGlyph(info.name))
-		glyph = Glyphs.font.glyphs[info.name]
-		for layer in glyph.layers:
-			for c in info.components:
-				layer.components.append(GSComponent(c.name))
-	else:
-		names.append(info.name)
+    if build_glyphs:
 
-print " ".join(names)
+        glyph = GSGlyph(info.name)
+        font.glyphs.append(GSGlyph(info.name))
+        glyph = Glyphs.font.glyphs[info.name]
+        for layer in glyph.layers:
+            for c in info.components:
+                layer.components.append(GSComponent(c.name))
+    else:
+        names.append(info.name)
+
+print(" ".join(names))
--- Glyphs/List All Possible Glyphs.py
+++ Glyphs/List All Possible Glyphs.py
@@ -1,4 +1,16 @@
+# MenuTitle: List All Possible Glyphs
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
+
 g = Glyphs.font.glyphs
 for i in GSGlyphsInfo.alloc().init().glyphInfos():
-    if i.components and i.name not in g and all(c.name in g for c in i.components):
-        print i.name
+    if (
+        i.components
+        and i.name not in g
+        and all(c.name in g for c in i.components)
+    ):
+        print(i.name)
