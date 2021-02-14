e694d78b66075a8478177ad8ea2e8243976650a5
Jens Kutilek <webmail@kutilek.de>
2021-02-14 12:52:42 +0100

    Use index and don't assign if no matching stem is found

--- TrueType/Assign Auto TT Stems.py
+++ TrueType/Assign Auto TT Stems.py
@@ -14,10 +14,13 @@ for layer in font.selectedLayers:
             if hint.stem == -1:
                 continue
             print(hint.stem)
+            o_idx = hint.originIndex()
+            t_idx = hint.targetIndex()
+            o_node = layer.paths[o_idx[0]].nodes[o_idx[1]]
+            t_node = layer.paths[t_idx[0]].nodes[t_idx[1]]
+            print(o_node, t_node)
             if hint.horizontal:
-                dist = abs(
-                    hint.originNode.position.y - hint.targetNode.position.y
-                )
+                dist = abs(o_node.y - t_node.y)
                 dists = {
                     abs(dist - s.width()): i
                     for i, s in enumerate(
@@ -26,9 +29,7 @@ for layer in font.selectedLayers:
                     if s.horizontal()
                 }
             else:
-                dist = abs(
-                    hint.originNode.position.x - hint.targetNode.position.x
-                )
+                dist = abs(o_node.x - t_node.x)
                 dists = {
                     abs(dist - s.width()): i
                     for i, s in enumerate(
@@ -36,7 +37,17 @@ for layer in font.selectedLayers:
                     )
                     if not s.horizontal()
                 }
+            print("Choosing", dist, "from", dists)
             min_value = sorted(dists.keys())[0]
-            hint.stem = dists[min_value]
+            if min_value < 20:
+                hint.stem = dists[min_value]
+            else:
+                print(
+                    "Could not find a matching stem for distance",
+                    dist,
+                    "in",
+                    dists,
+                )
+                print("Nodes:", o_node, t_node)
 
 font.enableUpdateInterface()
