b30ee2975c935b6b97ec6dc739d9a7871a88e642
Jens Kutilek <webmail@kutilek.de>
2021-02-27 18:18:44 +0100

    Remove some scripts

--- Hinting/Show index of selected nodes.py
+++
@@ -1,9 +0,0 @@
-# MenuTitle: Show Index Of Selected Nodes
-from __future__ import print_function
-
-print(Glyphs.font.selectedLayers[0].glyph.name)
-
-for path_index, path in enumerate(Glyphs.font.selectedLayers[0].paths):
-    for node_index, node in enumerate(path.nodes):
-        if node.selected:
-            print(path_index, node_index)
