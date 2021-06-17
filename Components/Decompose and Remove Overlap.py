16184adb7b2da01fb41980101c81ce072c9ba0e3
Jens Kutilek <webmail@kutilek.de>
2021-06-17 15:29:25 +0200

    Remove future imports

--- Components/Decompose and Remove Overlap.py
+++ Components/Decompose and Remove Overlap.py
@@ -1,10 +1,4 @@
 # MenuTitle: Decompose And Remove Overlap For Selection
-from __future__ import (
-    absolute_import,
-    division,
-    print_function,
-    unicode_literals,
-)
 
 for l in Glyphs.font.selectedLayers:
     l.decomposeComponents()
