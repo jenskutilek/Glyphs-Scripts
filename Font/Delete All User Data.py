cc73f1b0e3126af5a2fa6849e3fc23751564cfe9
Jens Kutilek <webmail@kutilek.de>
2021-08-26 17:10:26 +0200

    Update "Delete All User Data"

--- Font/Delete All User Data.py
+++ Font/Delete All User Data.py
@@ -1,27 +1,39 @@
-#MenuTitle: Delete All User Data
-from __future__ import absolute_import, division, print_function, unicode_literals
+# MenuTitle: Delete All User Data
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
 
-__doc__="""
-Delete all hidden user data from the current font, e.g. for cleaning up after importing a UFO.
-"""
+__doc__ = """Delete all hidden user data from the current font, e.g. for cleaning up after importing a UFO."""
 
-Glyphs.font.disableUpdateInterface()
+font = Glyphs.font
 
-for g in Glyphs.font.glyphs:
-	ud = g.userData
-	if ud:
-		print(g.name)
-		for key in ud.keys():
-			print("   ", key)
-			print("       ", ud[key])
-			del ud[key]
-	for layer in g.layers:
-		ud = layer.userData
-		if ud:
-			print(g.name)
-			for key in ud.keys():
-				print("   ", key)
-				print("       ", ud[key])
-				del ud[key]
+font.disableUpdateInterface()
 
-Glyphs.font.enableUpdateInterface()
+ud = font.userData
+if ud:
+    for key in list(ud.keys()):
+        print("   ", key)
+        print("       ", ud[key])
+        del ud[key]
+
+for g in font.glyphs:
+    ud = g.userData
+    if ud:
+        print(g.name)
+        for key in list(ud.keys()):
+            print("   ", key)
+            print("       ", ud[key])
+            del ud[key]
+    for layer in g.layers:
+        ud = layer.userData
+        if ud:
+            print(g.name)
+            for key in ud.keys():
+                print("   ", key)
+                print("       ", ud[key])
+                del ud[key]
+
+font.enableUpdateInterface()
