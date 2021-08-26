6e69cc098d4e958139004828cc2432487a9f4bf7
Jens Kutilek <webmail@kutilek.de>
2021-08-26 17:10:01 +0200

    Update

--- Components/Align components with background.py
+++ Components/Align components with background.py
@@ -2,9 +2,20 @@
 
 l = Glyphs.font.selectedLayers[0]
 for c in l.components:
-	cbox = c.bounds
-	for p in l.background.paths:
-		pbox = p.bounds
-		if cbox.size.width == pbox.size.width and cbox.size.height == pbox.size.height:
-			if cbox.origin.x != pbox.origin.x or cbox.origin.y != pbox.origin.y:
-				c.position = (c.position.x + pbox.origin.x - cbox.origin.x, c.position.y + pbox.origin.y - cbox.origin.y)
+    if c.automaticAlignment:
+        continue
+    cbox = c.bounds
+    for p in l.background.paths:
+        pbox = p.bounds
+        if (
+            cbox.size.width == pbox.size.width
+            and cbox.size.height == pbox.size.height
+        ):
+            if (
+                cbox.origin.x != pbox.origin.x
+                or cbox.origin.y != pbox.origin.y
+            ):
+                c.position = (
+                    c.position.x + pbox.origin.x - cbox.origin.x,
+                    c.position.y + pbox.origin.y - cbox.origin.y,
+                )
