97bf0fedb30d5bce3c84e52b836088468f91368f
Jens Kutilek <webmail@kutilek.de>
2023-01-13 16:12:26 +0100

    Minor

--- Components/Report Nested Components.py
+++ Components/Report Nested Components.py
@@ -14,7 +14,7 @@ for g in Glyphs.font.glyphs:
 			if hasattr(base, "components"):
 				if base.components and not base.paths:
 					if not flatten:
-						print(f"Nested component: {c.componentName} in glyph {g.name}")
+						print(f"Nested component: {c.componentName} in glyph {g.name} {l.name}")
 					if flatten:
 						if l.anchors:
 							print(f"Anchors existed in {g.name}, please fix manually")
