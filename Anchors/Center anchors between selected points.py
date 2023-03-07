5c1313ad56eadf4d7ff16529424cdf07cf49d700
Jens Kutilek <webmail@kutilek.de>
2023-03-07 15:22:48 +0100

    Center anchors in width if no nodes are selected

--- Anchors/Center anchors between selected points.py
+++ Anchors/Center anchors between selected points.py
@@ -11,7 +11,11 @@ from math import ceil
 selection = Layer.selection
 x = sorted([obj.x for obj in selection if type(obj) == GSNode])
 a = [obj for obj in selection if type(obj) == GSAnchor]
-if x and a:
-    ax = int(ceil((x[0] + x[-1]) / 2))
+if a:
+    if x:
+        ax = int(ceil((x[0] + x[-1]) / 2))
+    else:
+        # Center in width
+        ax = int(ceil(Layer.width / 2))
     for anchor in a:
         anchor.x = ax
