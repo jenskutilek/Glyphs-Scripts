dfad24e65cb4b90100c062a2e15f5a04df2e2f0e
Jens Kutilek <webmail@kutilek.de>
2026-06-04 20:07:22 +0200

    Only remove lines

--- Outlines/Remove Duplicate Nodes.py
+++ Outlines/Remove Duplicate Nodes.py
@@ -1,11 +1,15 @@
 # MenuTitle: Remove Duplicate Nodes
-from GlyphsApp import Glyphs
+from GlyphsApp import GSLINE, Glyphs
 
 for layer in Glyphs.font.selectedLayers:
     for path in layer.paths:
         remove_nodes = []
         for i, node in enumerate(path.nodes):
-            if node.prevNode.x == node.x and node.prevNode.y == node.y:
+            if (
+                node.type == GSLINE
+                and node.prevNode.x == node.x
+                and node.prevNode.y == node.y
+            ):
                 remove_nodes.append(i)
         for i in reversed(remove_nodes):
             del path.nodes[i]
